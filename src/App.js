c3ae1cd88901e4c04b9bd9cf2164e7aa2e537f49
maasym <github1@go-nihon.com>
2022-04-06 19:54:07 +0200

    featured

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
+import Best from "./components/best/Best";
+import Featured from "./components/featured/Featured";
 
 function App() {
   return (
     <div className="App">
     <Navbar />
-      <Hero />
+    <Hero />
+    <Best />
+    <Featured />
     </div>
   );
 }
